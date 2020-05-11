66d853269bfd7e563a1c0042e1472423146a2954
JACK <zhenya271@gmail.com>
2020-05-12 01:59:17 +0300

    adding build

---
+++ precache-manifest.790a465271d46e9dfa53bbdadfb60de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a4520819168acadcc8a1afecb9edcb",
+    "url": "/PhotoApp/index.html"
+  },
+  {
+    "revision": "bb7b9653a618a2f8c48c",
+    "url": "/PhotoApp/static/css/main.d8b67ead.chunk.css"
+  },
+  {
+    "revision": "1d5d70dccc8859f4388b",
+    "url": "/PhotoApp/static/js/2.6c228986.chunk.js"
+  },
+  {
+    "revision": "46c482ef0575d001a3428c51daa7f5e8",
+    "url": "/PhotoApp/static/js/2.6c228986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb7b9653a618a2f8c48c",
+    "url": "/PhotoApp/static/js/main.76bfa6bf.chunk.js"
+  },
+  {
+    "revision": "baecd4b94242cd585050",
+    "url": "/PhotoApp/static/js/runtime-main.048b0575.js"
+  }
+]);
